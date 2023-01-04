3f7e38364ab17fa392cf31b4bcaeca63b593e919
rovin-2 <rovin.mendoza@unah.hn>
2023-01-04 17:01:46 -0600

    metodo para generar el numero de la orden

--- src/main/java/com/proyecto/spring/service/IOrdenService.java
+++ src/main/java/com/proyecto/spring/service/IOrdenService.java
@@ -1,8 +1,11 @@
 package com.proyecto.spring.service;
 
+import java.util.List;
+
 import com.proyecto.spring.model.Orden;
 
 public interface IOrdenService {
 
+	List<Orden> findAll();
 	Orden save(Orden orden);
 }
