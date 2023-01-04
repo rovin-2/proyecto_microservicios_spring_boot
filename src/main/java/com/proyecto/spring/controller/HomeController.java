326fc4cb434eca0dc66fed52efa5ce4b428d47b0
rovin-2 <rovin.mendoza@unah.hn>
2023-01-04 11:02:28 -0600

    validacion doble producto y funcionalidad de boton carrito

--- src/main/java/com/proyecto/spring/controller/HomeController.java
+++ src/main/java/com/proyecto/spring/controller/HomeController.java
@@ -70,7 +70,13 @@ public class HomeController {
 		detalleOrden.setTotal(producto.getPrecio()*cantidad);
 		detalleOrden.setProducto(producto);
 		
-		detalles.add(detalleOrden);
+		//Validar que el producto no se anada dos veces
+		Integer idProducto = producto.getId();
+		boolean ingresado = detalles.stream().anyMatch(p -> p.getProducto().getId() == idProducto);
+		
+		if(!ingresado) {
+			detalles.add(detalleOrden);
+		}
 		
 		sumaTotal = detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
 		orden.setTotal(sumaTotal);
@@ -105,7 +111,13 @@ public class HomeController {
 		return "usuario/carrito";
 	}
 	
-	
+	@GetMapping("/getCart")
+	public String getCart(Model model) {
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+		
+		return "usuario/carrito";
+	}
 	
 	
 	
