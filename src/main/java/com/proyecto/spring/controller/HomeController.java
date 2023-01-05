ae5f101cca252f246baf8695c1c85d0bd6315ad1
rovin-2 <rovin.mendoza@unah.hn>
2023-01-04 20:01:39 -0600

    funcionalidad para buscar un producto

--- src/main/java/com/proyecto/spring/controller/HomeController.java
+++ src/main/java/com/proyecto/spring/controller/HomeController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -165,6 +167,17 @@ public class HomeController {
 		detalles.clear();
 
 		return "redirect:/";
+	}
+
+	@GetMapping("/search")
+	public String buscarProducto(@RequestParam String nombre, Model model) {
+		log.info("Nombre del producto: {}", nombre);
+		List<Producto> producto = productoService.findAll()
+				.stream().filter(p -> p.getNombre().contains(nombre))
+				.collect(Collectors.toList());
+		model.addAttribute("productos", producto);
+
+		return "usuario/home";
 	}
 
 }
